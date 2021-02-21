fd23947e310bbf74ea2b7ef6c478164dfe058c7b
23byte <dc23byte@163.com>
2021-02-21 16:27:43 +0800

    thinkphp的数据库与模型

--- ThinkPHP_5_1/tp5/application/controller/DataTest.php
+++ ThinkPHP_5_1/tp5/application/controller/DataTest.php
@@ -2,14 +2,24 @@
 namespace app\controller;
 
 
+use app\model\User;
 use think\Controller;
+use think\Db;
 
 class DataTest extends Controller
 {
     public function index()
     {
-
-        return'DBtest';
+        return'index';
+    }
+    public function getNoModelData()
+    {
+        //使用 table 方法要写上完整的表明 tp_user
+        $data = Db::name('user')->select();
+        return json($data);
+    }
+    public function getModelData(){
+        $data = User::select();
     }
 
 }
